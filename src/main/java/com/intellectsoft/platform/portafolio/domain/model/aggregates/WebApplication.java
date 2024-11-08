b8fd48ea7fa71a21186df9fe778cd139168db9d8
rivacortez <rivaldo062134@gmail.com>
2024-11-08 01:56:02 -0500

    feat: aggregate webApplication

--- src/main/java/com/intellectsoft/platform/portafolio/domain/model/aggregates/WebApplication.java
+++ src/main/java/com/intellectsoft/platform/portafolio/domain/model/aggregates/WebApplication.java
@@ -1,4 +1,97 @@
 package com.intellectsoft.platform.portafolio.domain.model.aggregates;
 
-public class WebApplication {
+import com.intellectsoft.platform.portafolio.domain.model.commands.CreateWebApplicationCommand;
+import com.intellectsoft.platform.portafolio.domain.model.events.ApplicationReadyEvent;
+import com.intellectsoft.platform.portafolio.domain.model.valueobjects.*;
+import com.intellectsoft.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+public class WebApplication extends AuditableAbstractAggregateRoot<WebApplication> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Size(max = 80)
+    private String name;
+
+    @NotNull
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "address", column = @Column(name = "frontend_address"))
+    })
+    private WebAddress frontendUrl;
+
+    @NotNull
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "address", column = @Column(name = "backend_address"))
+    })
+    private WebAddress backendUrl;
+
+    @NotNull
+    @Embedded
+    private ClientId clientId;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private FrontendStackName frontendStack;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private BackendStackName backendStack;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private CloudPlatformName cloudPlatform;
+
+    @Size(max = 360)
+    private String description;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date launchDate;
+
+    @NotNull
+    @Embedded
+    private AvailabilityPercentage availabilityPercentage;
+
+    @Column(name = "created_at", nullable = false, insertable = false, updatable = false)
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false, insertable = false, updatable = false)
+    private Date updatedAt;
+
+    public WebApplication() {}
+
+    public WebApplication(CreateWebApplicationCommand command, ClientId clientId) {
+        if (command == null || clientId == null) {
+            throw new IllegalArgumentException("Command and ClientId cannot be null");
+        }
+        this.name = command.name();
+        this.clientId = clientId;
+        this.frontendStack = command.frontendStack();
+        this.frontendUrl = command.frontendUrl();
+        this.backendStack = command.backendStack();
+        this.backendUrl = command.backendUrl();
+        this.cloudPlatform = command.cloudPlatform();
+        this.description = command.description();
+        this.launchDate = command.launchDate();
+        this.availabilityPercentage = new AvailabilityPercentage(command.availabilityPercentage());
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+        this.registerEvent(new ApplicationReadyEvent(this, this.clientId.clientId()));
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
