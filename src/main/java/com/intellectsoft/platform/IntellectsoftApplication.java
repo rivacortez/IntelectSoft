6d247cd209bcd0dd6d482825aa19f89426b1dd76
rivacortez <rivaldo062134@gmail.com>
2024-11-08 01:33:38 -0500

    chore: project setup

---
+++ src/main/java/com/intellectsoft/platform/IntellectsoftApplication.java
@@ -0,0 +1,13 @@
+package com.intellectsoft.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectsoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntellectsoftApplication.class, args);
+    }
+
+}
